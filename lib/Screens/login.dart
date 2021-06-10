39c595b0ed5248dc2d99a2821fc66f85bcfb39ca
Sarbin Shrestha <sarbin@supremeit.net>
2021-06-10 18:41:40 +0545

    chat ui, user signup/login with firebase and store data

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,6 +1,9 @@
+import 'package:chat_app/Screens/chat.dart';
 import 'package:chat_app/app_config/app_inputStyle.dart';
 import 'package:chat_app/widget/round_button.dart';
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class Login extends StatefulWidget {
   static const String id = 'login';
@@ -10,39 +13,84 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool showSpinner = false;
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: EdgeInsets.symmetric(horizontal: 24.0),child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Flexible(child: Hero(tag:'logo',child: Container(
-            height: 200.0,
-            child: Icon(Icons.message,size: 60.0,),
-          ),),),
-          SizedBox(height: 48.0,),
-        TextField(
-          keyboardType: TextInputType.emailAddress,
-          textAlign: TextAlign.center,
-          // onChanged: (value){
-          //   email = value;
-          // },
-          decoration:
-            kTextFieldDecoration.copyWith(hintText: 'Enter Your email'),
-
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Icon(
+                      Icons.message,
+                      size: 60.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Enter Your email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter you password'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundButton(
+                  color: Colors.lightBlueAccent,
+                  title: 'Log In',
+                  onPresses: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      if (user != null) {
+                        Navigator.pushNamed(context, Chat.id);
+                      }
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
+                  })
+            ],
+          ),
         ),
-          SizedBox(
-            height: 8.0,
-          ),
-          TextField(obscureText: true,
-            textAlign: TextAlign.center,
-            decoration: kTextFieldDecoration.copyWith(hintText: 'Enter you password'),
-          ),
-          SizedBox(height: 24.0,),
-          RoundButton(color:Colors.lightBlueAccent,title: 'Log In',onPresses: (){})
-        ],
-      ),),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/Screens/chat.dart';
 import 'package:chat_app/Screens/login.dart';
 import 'package:chat_app/Screens/register.dart';
 import 'package:chat_app/Screens/welcome.dart';
@@ -6,15 +7,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(ChatApp());
 
 class ChatApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: Welcome.id,
       routes: {
         Welcome.id: (context) => Welcome(),
-        Login.id:(context) => Login(),
-        Register.id:(context) => Register(),
+        Login.id: (context) => Login(),
+        Register.id: (context) => Register(),
+        Chat.id: (context) => Chat(),
       },
     );
   }
